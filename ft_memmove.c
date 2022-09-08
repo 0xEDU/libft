ec7bd2dcf95e9b91c0b380a66039d34d978de1f8
etachott <etachott@42sp.org.br>
2022-09-08 19:33:24 +0200

    solved memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 14:56:08 by coder             #+#    #+#             */
-/*   Updated: 2022/09/08 16:36:02 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/08 19:18:44 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
+	char		*d;
+	const char	*s;
 
-	i = 0;
-	if (array2 in array1)
-		while (not overlap or len--)
-			last_pos_arr1 = last_pos_arr2;
+	d = dst;
+	s = src;
+	if (dst > src)
+		while (len--)
+			*(d + len) = *(s + len);
 	else
-		ft_memset(dst, src, len);
+		ft_memcpy(dst, src, len);
+	return (dst);
 }
