86f86931827ca9ebfe91518b42bcb081df973d4d
etachott <etachott@42sp.org.br>
2022-09-10 17:50:19 +0200

    a'

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,50 +6,23 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 21:30:25 by coder             #+#    #+#             */
-/*   Updated: 2022/08/31 20:04:33 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/08 21:09:23 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*returns(size_t len, char *pos, const char *hay, const char *n);
+//atic char	*reverse_len(const char *haystack, const char *needle, size_t len);
+//atic char	*returns(size_t len, char *pos, const char *hay, const char *n);
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	hit;
-	char	*pos;
+	size_t	jj;
+	char	*h;
 
-	hit = 0;
-	pos = NULL;
-	if (*needle == '\0')
-		return ((char *) haystack);
-	while (len--)
-	{
-		if (*haystack == '\0')
-			break ;
-		if (*haystack == *needle)
-		{
-			hit = 1;
-			pos = (char *) haystack;
-			break ;
-		}
-		haystack++;
-	}
-	if (hit)
-		return (returns(len, pos, haystack, needle));
-	else
-		return (NULL);
-}
-
-static char	*returns(size_t len, char *pos, const char *hay, const char *n)
-{
-	while (*n == *hay && len--)
-	{
-		n++;
-		hay++;
-	}
-	if (*n)
-		return (NULL);
-	else
-		return (pos);
+	jj = len;
+	h = (char *) haystack;
+	if (ft_strchr(haystack, needle[0]))
+		return (h);
+	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 01:18:56 by coder             #+#    #+#             */
-/*   Updated: 2022/08/30 01:43:47 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/08 19:39:06 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ char	*ft_strrchr(const char *s, int c)
 	len = ft_len(s);
 	while (len)
 	{
-		if (s[len] == c)
+		if (s[len] == (char) c)
 			return ((char *)s + len);
 		else
 			len--;
 	}
-	if (s[len] == c)
+	if (s[len] == (char) c)
 		return ((char *)s + len);
 	return (0);
 }
