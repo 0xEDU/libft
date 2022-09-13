2aa48844bba49845802ee5f550421adece5307ca
etachott <etachott@42sp.org.br>
2022-09-13 21:15:17 +0200

    calloc!

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:47:16 by coder             #+#    #+#             */
-/*   Updated: 2022/09/11 12:57:20 by etachott         ###   ########.fr       */
+/*   Updated: 2022/09/13 21:13:44 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	*ft_calloc(size_t count, size_t size)
 	i = 0;
 	if (size > 2147483647 / count)
 		return (NULL);
-	if (count == 0 || size == 0)
-		return (malloc(0));
 	p = malloc(count * size);
+	if (p = NULL)
+		return (p);
 	while (i < size * count)
 		p[i++] = '\0';
 	return (p);
