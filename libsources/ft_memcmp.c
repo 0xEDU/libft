4a2a7e609c119af19d2f6b99c13a37fe35447e24
etachott <etachott@student.42sp.or.br>
2023-03-16 15:16:23 -0300

    docs: added documentation for memcmp

--- libsources/ft_memcmp.c
+++ libsources/ft_memcmp.c
@@ -6,12 +6,22 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 00:58:08 by coder             #+#    #+#             */
-/*   Updated: 2023/03/16 15:03:11 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 15:16:06 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+*	Compare two blocks of memory.
+*
+*	@param void	*s1: A pointer to the first block of memory
+*	@param void	*s2: A pointer to the second block of memory
+*	@param size_t	n: the number of bytes to compare
+*
+*	@return	If the two memory blocks are equal, return 0. Else, return the
+*	difference between the two blocks.
+*/
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t				index;
