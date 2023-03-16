b4a6932fa7b0353980785419dfe5329e56dec2b9
etachott <etachott@student.42sp.or.br>
2023-03-16 12:20:23 -0300

    docs: added documentation for atoi_hex

--- libsources/ft_atoi_hex.c
+++ libsources/ft_atoi_hex.c
@@ -6,12 +6,20 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 13:42:36 by etachott          #+#    #+#             */
-/*   Updated: 2023/03/16 11:44:26 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 12:20:04 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+ * Convert a string representation of a hexadecimal number to an integer.
+ * NOTE: If the input string is greater than MAX_INT, it WILL overflow.
+ *
+ * @param char	*str: Pointer to a string.
+ *
+ * @return int	result: A integer that corresponds to the converted string.
+*/
 int	ft_atoi_hex(const char *str)
 {
 	int		index;
