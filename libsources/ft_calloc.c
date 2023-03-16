ff67d7856eff626fa5d0afbf12d1bc2452bc7305
etachott <etachott@student.42sp.or.br>
2023-03-16 12:50:40 -0300

    docs: added documentation for calloc

--- libsources/ft_calloc.c
+++ libsources/ft_calloc.c
@@ -6,12 +6,20 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:47:16 by coder             #+#    #+#             */
-/*   Updated: 2023/03/16 12:39:42 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 12:50:06 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+*	Calloc allocates count * size bytes in memory and set it to 0.
+*
+*	@param size_t	count: Quantity of elements.
+*	@param size_t	size: Size of each element.
+*
+*	@return Pointer to allocated memory block.
+*/
 void	*ft_calloc(size_t count, size_t size)
 {
 	size_t			index;
