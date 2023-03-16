46ffc521b95ea22ee23d337410d1bd574ba2d671
etachott <etachott@student.42sp.or.br>
2023-03-16 17:48:59 -0300

    refactor: removed unnecessary function and else statement

--- libsources/ft_strrchr.c
+++ libsources/ft_strrchr.c
@@ -6,37 +6,24 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 01:18:56 by coder             #+#    #+#             */
-/*   Updated: 2022/09/08 19:39:06 by coder            ###   ########.fr       */
+/*   Updated: 2023/03/16 17:48:16 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_len(const char *s);
-
 char	*ft_strrchr(const char *s, int c)
 {
 	int	len;
 
-	len = ft_len(s);
+	len = ft_strlen(s);
 	while (len)
 	{
 		if (s[len] == (char) c)
 			return ((char *)s + len);
-		else
-			len--;
+		len--;
 	}
 	if (s[len] == (char) c)
 		return ((char *)s + len);
 	return (0);
-}
-
-static int	ft_len(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
 }
