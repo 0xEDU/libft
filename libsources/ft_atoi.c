17dc181a7751cdb7765fc40e4d5931e3a3aad43f
etachott <etachott@student.42sp.or.br>
2023-03-16 11:39:42 -0300

    refactor: removed magic numbers

--- libsources/ft_atoi.c
+++ libsources/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 19:54:52 by coder             #+#    #+#             */
-/*   Updated: 2023/03/16 11:34:47 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 11:39:23 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ int	ft_atoi(const char *str)
 	while (*str == ' ' || *str == '\t' || *str == '\r'
 		|| *str == '\n' || *str == '\f' || *str == '\v')
 		str++;
-	if (*str == 43 || *str == 45)
+	if (*str == '+' || *str == '-')
 	{
-		if (*str == 45)
+		if (*str == '-')
 			is_negative = -1;
 		str++;
 	}
-	while (48 <= *str && *str <= 57)
+	while ('0' <= *str && *str <= '9')
 	{
-		result = (*str - 48) + (result * 10);
+		result = (*str - '0') + (result * 10);
 		str++;
 	}
 	return (result * is_negative);
