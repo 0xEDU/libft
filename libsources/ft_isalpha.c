3f97764fca3b43de1ed7acef73c2f5299c49ae8a
etachott <etachott@student.42sp.or.br>
2023-03-16 14:00:46 -0300

    docs: added documentation for isalpha

--- libsources/ft_isalpha.c
+++ libsources/ft_isalpha.c
@@ -6,12 +6,20 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 15:51:34 by coder             #+#    #+#             */
-/*   Updated: 2023/03/16 13:57:27 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 14:00:10 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+*       Return a non-zero value if c is a letter.
+*
+*       @param int      c: The integer value to check.
+*
+*       @return If the character is a letter return a non-zero value
+*       else, return 0.
+*/
 int	ft_isalpha(int c)
 {
 	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
