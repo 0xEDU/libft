4804afb21f4514b37fd9c004e68c29452d8eeb77
etachott <etachott@student.42sp.or.br>
2023-03-16 18:03:46 -0300

    docs: fixed grammar

--- libsources/ft_tolower.c
+++ libsources/ft_tolower.c
@@ -6,15 +6,16 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:11:17 by coder             #+#    #+#             */
-/*   Updated: 2023/03/16 18:00:05 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 18:03:20 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 /*
 *	Converts an uppercase letter to lowercase.
 *
-*	@param c The character to convert.
+*	@param int	c: The character to convert.
 *
 *	@return The lowercase version of the input character if it is an uppercase
 *	letter, or the input character otherwise.
