04be6718f747a55fb1b33bba7b8d8e3c2b660fa6
etachott <etachott@student.42sp.or.br>
2023-03-16 16:02:27 -0300

    chore: normie

--- libsources/ft_memchr.c
+++ libsources/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 00:22:39 by coder             #+#    #+#             */
-/*   Updated: 2023/03/16 14:59:06 by edu              ###   ########.fr       */
+/*   Updated: 2023/03/16 16:02:06 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@
 */
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned	char	*ptr = s;
+	const unsigned char	*ptr = s;
 
 	c = (unsigned char) c;
 	while (n--)
 	{
 		if (*ptr == c)
-			return (void *)ptr;
+			return ((void *)ptr);
 		ptr++;
 	}
 	return (NULL);
